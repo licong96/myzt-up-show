67455a1948e304b03c042ca3846bb7569ce53799
licong96 <licong19961204@gmail.com>
2017-09-08 17:41:51 +0800

    修改数据请求方式

--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -259,7 +259,9 @@
       data () {         // 监听数据变化
         this.loadImage()
       },
-      '$route': 'getData'     // 如果路由有变化，会再次执行该方法
+      $route () {         // 如果路由有变化，会再次执行该方法
+        this.getData()
+      }
     }
   }
 </script>
